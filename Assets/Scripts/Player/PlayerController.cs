3bc0a677b8c9e5b980430a6377accb19d9eff894
1PKure <pulidomatias@gmail.com>
2025-06-09 21:35:16 -0300

    Jump + Invisible Walls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,10 +13,16 @@ public class PlayerController : MonoBehaviour
     private Camera activeCamera;
     private float mouseX = 0f;
     private float mouseY = 0f;
-    private MovementLimiter movementLimiter;
+    private Vector3 velocity;
+    private bool isGrounded;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
     void Start()
     {
-        movementLimiter = GetComponent<MovementLimiter>();
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
@@ -42,13 +48,19 @@ public class PlayerController : MonoBehaviour
 
     void HandleMovement()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         Vector3 move = transform.right * x + transform.forward * z;
         move *= playerData.moveSpeed * Time.deltaTime;
-
-        if (movementLimiter != null)
-            move = movementLimiter.LimitMovement(move);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
         controller.Move(move);
     }
